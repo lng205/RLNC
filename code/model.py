a230775ce68b0e9aacb6ffca036d20d3adccb0b3
lng205 <lng205.yb@gmail.com>
2024-08-01 17:27:31 +0800

    fix bugs; improve printing format; redesign the decoding process; add logic to handle corner cases at the encoder;

--- code/model.py
+++ code/model.py
@@ -38,6 +38,12 @@ class Packet:
         syms = GF(list(data[16:]))
         return cls(sourceid, repairid, win_s, win_e, coes, syms)
 
+    def __str__(self) -> str:
+        if self.sourceid >= 0:
+            return f"Source Packet {self.sourceid}"
+        else:
+            return f"Repair Packet [{self.win_s}, {self.win_e})"
+
 
 class Encoder:
     def __init__(self, repfreq: int):
@@ -45,31 +51,31 @@ class Encoder:
         self.srcpkt: list[bytes] = []
         self.nextsid: int = 0
         self.acksid: int = -1
-        self.rcount: int = 0
+        self.rcount: int = -1
 
     def enqueue(self, data: bytes):
         self.srcpkt.append(data)
 
-    def generate_packet(self):
-        if self.nextsid + 1 >= len(self.srcpkt) or \
-            random.random() < self.repfreq:
-            return self.output_repair_packet()
+    def generate_packet(self) -> Packet:
+        all_acked: bool = self.acksid + 1 == self.nextsid
+        all_sent: bool = self.nextsid == len(self.srcpkt)
+
+        if all_acked:
+            pkt = self._output_source_packet(self.nextsid)
+            self.nextsid += 1
+        elif random.random() < self.repfreq or all_sent:
+            self.rcount += 1
+            pkt = self._output_repair_packet(self.rcount, self.acksid + 1, self.nextsid)
         else:
-            return self.output_source_packet()
+            pkt = self._output_source_packet(self.nextsid)
+            self.nextsid += 1
+        return pkt
 
-    def output_source_packet(self):
-        self.nextsid += 1
-        syms = GF(list(self.srcpkt[self.nextsid]))
-        return Packet(self.nextsid, -1, -1, -1, None, syms)
+    def _output_source_packet(self, sourceid: int) -> Packet:
+        syms = GF(list(self.srcpkt[sourceid]))
+        return Packet(sourceid, -1, -1, -1, None, syms)
 
-    def output_repair_packet(self):
-        """The encoding window is [acksid+1, nextsid)."""
-        self.rcount += 1
-
-        win_s = self.acksid + 1
-        win_e = self.nextsid
-        repairid = self.rcount
-
+    def _output_repair_packet(self, repairid, win_s, win_e) -> Packet:
         coes = GF.Random(win_e-win_s, seed=repairid).reshape(1, -1)
         syms_all = GF([list(row) for row in self.srcpkt[win_s:win_e]])
         syms = coes @ syms_all
@@ -85,79 +91,100 @@ class Decoder:
         self.inorder = -1
         self.recovered:list[galois.FieldArray] = []
         self.active = False
-
-    def receive_packet(self, pkt: Packet) -> None:
-        if self.is_outdated(pkt):
-            return
-        if self.active:
-            self.process_packet(pkt)
-        elif pkt.sourceid == self.inorder + 1:
-            self.recovered.append(pkt.syms)
-            self.inorder += 1
-        else:
-            self.activate(pkt)
-
-    def is_outdated(self, pkt: Packet) -> bool:
-        if pkt.sourceid >= 0:
-            return pkt.sourceid <= self.inorder
-        else:
-            return pkt.win_e <= self.inorder + 1
-
-    def activate(self, pkt: Packet) -> None:
-        if pkt.sourceid >= 0:
-            win_e = pkt.sourceid + 1
-            coes = GF(np.pad([1], (pkt.sourceid - self.inorder - 1, 0)))
-        else:
-            win_e = pkt.win_e
-            coes = pkt.coes
-
-        self.active = True
-        self.win_s = self.inorder + 1
-        self.win_e = win_e
-        self.coes = coes.reshape(1, -1)
-        self.messages = pkt.syms
-
-    def process_packet(self, pkt: Packet) -> None:
-        if pkt.sourceid >= 0:
-            extend_length = pkt.sourceid - self.win_e + 1
-            if extend_length > 0:
-                self.coes = GF(np.pad(self.coes, ((0, 0), (0, extend_length))))
-                self.win_e = pkt.sourceid + 1
-            pad_left = pkt.sourceid - self.win_s
-            pad_right = self.win_e - pkt.sourceid - 1
-            coes = GF(np.pad([[1]], ((0, 0), (pad_left, pad_right))))
-            syms = pkt.syms
-        else:
-            coes = pkt.coes.reshape(1, -1)
-            syms = pkt.syms
-            width = self.win_s - pkt.win_s
-            if width > 0:
-                A = coes[:, :width]
-                B = self.recovered[pkt.win_s:self.win_s]
-                syms = syms - (GF(A) @ GF(B)).reshape(-1)
-                coes = coes[:, width:]
-
-            if pkt.win_e > self.win_e:
-                self.coes = GF(np.pad(self.coes, ((0, 0), (0, pkt.win_e-self.win_e))))
-                self.win_e = pkt.win_e
-            elif pkt.win_e < self.win_e:
-                coes = GF(np.pad(coes, ((0, 0), (0, self.win_e-pkt.win_e))))
-
-        coes_new = GF(np.vstack((self.coes, coes)))
-        if np.linalg.matrix_rank(coes_new) == coes_new.shape[0]:
-            self.coes = coes_new
-        self.messages = GF(np.vstack((self.messages, syms)))
-
-        if self.coes.shape[0] == self.coes.shape[1]:
-            self.deactivate()
-
-    def deactivate(self) -> None:
-        decoded = np.linalg.inv(self.coes) @ self.messages
-        for row in decoded:
-            self.recovered.append(row)
-        self.inorder = len(self.recovered) - 1
-        self.active = False
         self.win_s = -1
         self.win_e = -1
         self.coes = None
         self.messages = None
+
+    def receive_packet(self, pkt: Packet) -> None:
+        if self._is_outdated(pkt):
+            return
+        if self.active:
+            self._process_packet(pkt)
+        elif pkt.sourceid == self.inorder + 1:
+            self.recovered.append(pkt.syms)
+            self.inorder += 1
+        else:
+            self._activate(pkt)
+
+    def _is_outdated(self, pkt: Packet) -> bool:
+        if pkt.sourceid >= 0:
+            return pkt.sourceid <= self.inorder
+        else:
+            return pkt.win_e <= self.inorder + 1 and pkt.win_s <= self.inorder
+
+    def _activate(self, pkt: Packet) -> None:
+        if pkt.sourceid >= 0:
+            self.active = True
+            self.win_s = self.inorder + 1
+            self.win_e = pkt.sourceid + 1
+            coes = GF(np.pad([1], (pkt.sourceid - self.inorder - 1, 0)))
+            self.coes = coes.reshape(1, -1)
+            self.messages = pkt.syms
+        else:
+            coes, syms = self._process_repair_packet(pkt)
+            if len(coes) == 1:
+                self.recovered.append(syms / coes[0])
+                self.inorder = pkt.win_e - 1
+                return
+
+            self.active = True
+            self.win_s = self.inorder + 1
+            self.win_e = pkt.win_e
+            self.coes = coes.reshape(1, -1)
+            self.messages = syms
+
+    def _process_packet(self, pkt: Packet) -> None:
+        if pkt.sourceid >= 0:
+            pad_left = pkt.sourceid - self.win_s
+            pad_right = max(0, self.win_e - pkt.sourceid - 1)
+            coes = GF(np.pad([1], (pad_left, pad_right)))
+            syms = pkt.syms
+        else:
+            coes, syms = self._process_repair_packet(pkt)
+
+        # extend the decoding window
+        delta = len(coes) - self.coes.shape[1]
+        if delta > 0:
+            self.coes = GF(np.pad(self.coes, ((0, 0), (0, delta))))
+            self.win_e = self.win_e + delta
+
+        coes_new = GF(np.vstack((self.coes, coes.reshape(1, -1))))
+        if np.linalg.matrix_rank(coes_new) != coes_new.shape[0]:
+            # linearly dependent
+            return
+
+        self.coes = coes_new
+        self.messages = GF(np.vstack((self.messages, syms)))
+
+        if self.coes.shape[0] == self.coes.shape[1]:
+            self.active = False
+            decoded = np.linalg.inv(self.coes) @ self.messages
+            for row in decoded:
+                self.recovered.append(row)
+            self.inorder = len(self.recovered) - 1
+            self.win_s = -1
+            self.win_e = -1
+            self.coes = None
+            self.messages = None
+
+    def _process_repair_packet(self, pkt: Packet) -> tuple[galois.FieldArray, galois.FieldArray]:
+        win_s = self.inorder + 1
+        cut_left = win_s - pkt.win_s
+        if cut_left > 0:
+            A = pkt.coes[:cut_left].reshape(1, -1)
+            B = self.recovered[pkt.win_s:win_s]
+            coes = pkt.coes[cut_left:]
+            syms = pkt.syms - (GF(A) @ GF(B)).reshape(-1)
+        else:
+            coes = pkt.coes
+            syms = pkt.syms
+
+        pad_left = max(0, pkt.win_s - win_s)
+        if self.win_e >= 0:
+            pad_right = max(0, self.win_e - pkt.win_e)
+        else:
+            pad_right = 0
+        coes = GF(np.pad(coes, (pad_left, pad_right)))
+
+        return coes, syms
--- code/mystreamc.py
+++ code/mystreamc.py
@@ -37,33 +37,39 @@ def main():
 
     while dc.inorder < snum-1:
         slot += 1
-        pktstr = ec.generate_packet().serialize()
 
+        # enqueue to the cyclic buffer
         pos1 = slot % (Tp+1)
+        pkt = ec.generate_packet()
         if random.random() < epsilon:
             queue[pos1] = None
+            print(f"[Channel]   Packet at slot {slot} is lost")
         else:
-            queue[pos1] = pktstr
+            queue[pos1] = pkt.serialize()
+            print(f"[Channel]   {str(pkt)} is sent")
 
+        # delayed receiving
         pos2 = (slot-Tp) % (Tp+1)
         if queue[pos2] is not None:
-            rpkt = Packet.deserialize(queue[pos2])
-            dc.receive_packet(rpkt)
+            dc.receive_packet(Packet.deserialize(queue[pos2]))
             queue[pos2] = None
 
+        # lossless feedback
         if dc.inorder >= 0 and slot >= Tp:
             feedback[pos1] = dc.inorder
+            print(f"[Feedback]  Acknowledgement for {feedback[pos1]} is sent")
             if slot >= Tp and feedback[pos2] is not None:
                 ec.flush_acked_packets(feedback[pos2])
                 feedback[pos2] = None
 
+    # check the correctness
     correct = True
     for i in range(snum):
         if data[i*PKTSIZE:(i+1)*PKTSIZE] != dc.recovered[i].tobytes():
             correct = False
-            print()
+            print(f"[Summary]   Source packet {i} is not identical to the original")
     if correct:
-        print("[Summary] All source packets are recovered correctly")
+        print("[Summary]    All source packets are recovered correctly")
 
 if __name__ == "__main__":
     main()
